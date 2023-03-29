e469e4e27d363742edc9620da32c85baf7fe60ff
voleti-sruthi <sruthi.lakshmi@m2pfintech.com>
2023-03-29 22:54:17 +0530

    req1 Add. Included Logic and TestCases for checking FizzBuzz from 1 to 100

--- src/main/java/com/m2pfintech/FizzBuzz.java
+++ src/main/java/com/m2pfintech/FizzBuzz.java
@@ -1,4 +1,20 @@
 package com.m2pfintech;
 
 public class FizzBuzz {
+    private boolean checkValidity(int inputNumber) {
+        return inputNumber > 0 && inputNumber <= 100;
+    }
+
+    public String checkFizzBuzz(int inputNumber) {
+        if (checkValidity(inputNumber)) {
+            if (inputNumber % 3 == 0) {
+                return "FIZZ";
+            } else if (inputNumber % 5 == 0) {
+                return "BUZZ";
+            }
+            return String.valueOf(inputNumber);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 }
--- src/test/java/com/m2pfintech/FizzBuzzTest.java
+++ src/test/java/com/m2pfintech/FizzBuzzTest.java
@@ -1,5 +1,50 @@
 package com.m2pfintech;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class FizzBuzzTest {
     FizzBuzz fizzBuzzObject = new FizzBuzz();
+
+    @Test
+    void toReturnFIZZWhenInputIsDivisibleByThree(){
+        String expectedValue = "FIZZ";
+
+        String actualValue = fizzBuzzObject.checkFizzBuzz(3);
+
+        assertEquals(expectedValue,actualValue);
+    }
+
+    @Test
+    void toReturnBUZZWhenInputIsDivisibleByFive(){
+        String expectedValue = "BUZZ";
+
+        String actualValue = fizzBuzzObject.checkFizzBuzz(5);
+
+        assertEquals(expectedValue,actualValue);
+    }
+
+    @Test
+    void toReturnTheNumberWhenInputNumberIsNeitherDivisibleByThreeNorFive(){
+        String expectedValue = "17";
+
+        String actualValue = fizzBuzzObject.checkFizzBuzz(17);
+
+        assertEquals(expectedValue,actualValue);
+    }
+    @Test
+    void toThrowIllegalValueExceptionInCaseOfNegativeInputNumbers(){
+        assertThrows(IllegalArgumentException.class,()->fizzBuzzObject.checkFizzBuzz(-21));
+    }
+
+    @Test
+    void toThrowIllegalValueExceptionInCaseOfLargeInputNumbers(){
+        assertThrows(IllegalArgumentException.class,()->fizzBuzzObject.checkFizzBuzz(121));
+    }
+
+
+
+
 }
